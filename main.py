d4b9913042b96b40e9bd1386c58edf27e74753a5
Nitish <nitishsharma.sharma2@gmail.com>
2024-09-26 00:18:19 +0530

    adding env variable

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from services.datasource_connector import Connector
+from sqlalchemy import text
+from dotenv import load_dotenv
 
+load_dotenv() #laod the env variables
 connector = Connector('localhost',3306)
 conn = connector.create_connection()
-print(conn)
+with conn.connect() as connection:
+    result = connection.execute(text("SELECT * FROM orders"))
+    tables = result.fetchall()
+    print(tables)
--- services/datasource_connector.py
+++ services/datasource_connector.py
@@ -6,10 +6,10 @@ class Connector:
     def __init__(self,host,port):
         self.host = host
         self.port = port
-        self.user = os.environ.get('USERNAME')
-        self.password = os.environ.get('PASS')
+        self.user = os.getenv("USER")
+        self.password = os.getenv("PASS")
     def create_connection(self):
         connection_string=f'mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/dev01'
-        engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+        engine = create_engine(connection_string, echo=True)
         return engine
 
