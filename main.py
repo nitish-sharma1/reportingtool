e9ff4cbe53fa2ccba247505ea22edce637c3a9ed
Nitish <nitishsharma.sharma2@gmail.com>
2024-10-02 01:40:31 +0530

    adding encryption service for the password

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from services.datasource_connector import Connector
 from sqlalchemy import text
+from services.encryptionservice.encryptionservice import Encryption_Service
 from dotenv import load_dotenv
 
 load_dotenv() #laod the env variables
+
 connector = Connector(database_type="postgres")
 conn = connector.create_engine('reprotingdb')
 with conn.connect() as connection:
---
+++ services/encryptionservice/encryptionservice.py
@@ -0,0 +1,17 @@
+from cryptography.fernet import Fernet
+import os
+
+
+class Encryption_Service:
+    def __init__(self):
+        self.key = os.getenv('ENCRYPTION_SECRET').encode('utf-8')
+
+    def encrypt(self, plaintext):
+        f = Fernet(self.key)
+        token = f.encrypt(plaintext.encode('utf-8'))
+        return token
+
+    def decrypt(self, encoded_string):
+        f = Fernet(self.key)
+        plain_text = f.decrypt(encoded_string.encode('utf-8'))
+        return plain_text.decode('utf-8')
