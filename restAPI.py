071e6933817ae0e57d0dff53d455511b2bf09834
Nitish <nitishsharma.sharma2@gmail.com>
2024-10-15 00:11:36 +0530

    making recipent , subject , email_body as not mandatory feilds

--- restAPI.py
+++ restAPI.py
@@ -14,9 +14,9 @@ class ConfigSchema(Schema):
     frequency = fields.List(fields.Str(), required=True)
     report_name = fields.Str(required=True)
     query = fields.Str(required=True)
-    recipents = fields.List(fields.Str(), required=True)
-    subject = fields.Str(required=True)
-    email_body = fields.Str(required=True)
+    recipents = fields.List(fields.Str(), required=False)
+    subject = fields.Str(required=False)
+    email_body = fields.Str(required=False)
     transfer_type = fields.Str(required=True)
 
     class Meta:
