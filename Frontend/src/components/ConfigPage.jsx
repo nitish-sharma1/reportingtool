6747fbdd5eaf1a4bf74c852a0ab26a08618cc67c
Nitish <nitishsharma.sharma2@gmail.com>
2024-10-20 16:35:40 +0530

    added complete configpage FE

--- Frontend/src/components/ConfigPage.jsx
+++ Frontend/src/components/ConfigPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './NavBar'
+import ConfigPageBody from './ConfigPageBody'
 
 function ConfigPage() {
   return (
-    <div className='bg-off-white w-full h-screen'>
+    <div className='bg-off-white flex flex-col h-screen'>
         <NavBar></NavBar>
-    
+        <ConfigPageBody></ConfigPageBody>
+
     
     </div>
   )
--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Logo from './Logo'
 import { GiHamburgerMenu } from "react-icons/gi";
 import SearchBar from './SearchBar';
+import NavUserSection from './NavUserSection';
 
 function NavBar() {
   return (
-    <div className='bg-white flex  items-center h-max p-2'>
-        <div className='w-44 ml-16'> <Logo ></Logo></div>
-        <span className='mx-4 '><GiHamburgerMenu size={20} /></span>
+    <div className='bg-white flex w-full justify-between items-center h-max p-2'>
+        <div className='w-52 ml-8 flex items-center'> <Logo ></Logo> <span className='mx-4 '><GiHamburgerMenu size={20} /></span></div>
+        
         <SearchBar></SearchBar>
+        <div className=' flex justify-end '><NavUserSection></NavUserSection></div>
         
         
         </div>
