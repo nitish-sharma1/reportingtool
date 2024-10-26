e9a69ff66f1d4f348f40e16cf47f9b16dd836748
Nitish <nitishsharma.sharma2@gmail.com>
2024-10-26 23:24:31 +0530

    fixed the data source schema

--- Schema/datasourceschema.py
+++ Schema/datasourceschema.py
@@ -3,11 +3,11 @@ from marshmallow import Schema, fields
 
 class DataSourceSchema(Schema):
     datasource_type = fields.Str(required=True)
-    Instance_name = fields.Str(required=True)
+    instance_name = fields.Str(required=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True)
     hostname = fields.Str(required=True)
-    port = fields.Str(required=True)
+    port = fields.Int(required=True)
 
     class Meta:
         strict = True
