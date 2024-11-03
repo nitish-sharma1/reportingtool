16754e89a402c46ab4215e39c9ceaaac94118ebf
Nitish <nitishsharma.sharma2@gmail.com>
2024-11-04 02:55:21 +0530

    adding new fields in Add Reports

--- Frontend/src/components/sevicesCards/AddReportService.jsx
+++ Frontend/src/components/sevicesCards/AddReportService.jsx
@@ -4,11 +4,26 @@ import React, { useEffect, useState } from 'react'
 function AddReportService() {
     const [btnState,setBtnState] = useState(false)
   const [dataSourceName , setDataSource] = useState('')
-  const [instanceName , setInstanceName] = useState('')
+  const [instanceName , getInstanceName] = useState([])
   const [username , setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [hostname,setHostname] = useState('')
   const [port , setPort] = useState('')
+
+
+  useEffect(() => {axios.get(import.meta.env.VITE_GET_INSTANCE_ENDPOINT)
+    .then(
+    response => {console.log(response.data)
+    getInstanceName(response.data.instance_names)
+    }
+        
+).catch(error => {console.log(error)})},[])
+
+
+
+
+
+
   useEffect(() => {
     if(btnState){
     axios.post(import.meta.env.VITE_ADD_REPORT_ENDPOINT,{
@@ -37,55 +52,50 @@ function AddReportService() {
       <div className='flex bg-white p-20 flex-col items-center justify-center rounded-md'>
      
         <h1 className="mb-4 text-2xl font-bold">Configure A New Report</h1> 
-        <input 
-          type="text" 
-          placeholder="Report Name" 
-          className="block w-96 p-2 mb-4 border border-gray-300 bg-input rounded" 
-          required  onChange={(e)=> setInstanceName(e.target.value)}
-        />
-        
-        <select className="block w-64 p-2 mb-4 border border-gray-300 bg-input rounded " required onChange={(e)=> setDataSource(e.target.value)} >
-          <option value="">Select Instance Name</option>
-          <option value="mysql">MySQL</option>
-          <option value="postgresql">PostgreSQL</option>
-          <option value="mssql">MsSQL</option>
-        </select>
-        <input 
-          type="text" 
-          placeholder="Instance name" 
-          className="block w-96 p-2 mb-4 border border-gray-300 bg-input rounded" 
-          required  onChange={(e)=> setInstanceName(e.target.value)}
-        />
         <div className='flex'>
         <input 
           type="text" 
-          placeholder="Username" 
+          placeholder="Report Name" 
+          className="block w-64 p-2 mb-4 border border-gray-300 bg-input rounded" 
+          required  onChange={(e)=> setInstanceName(e.target.value)}
+        />
+        
+        <select className="block w-64 p-2 mb-4 border border-gray-300  ml-2 bg-input rounded " required onChange={(e)=> setDataSource(e.target.value)} >
+          <option value="">Select Instance Name</option>
+          {instanceName.map((name, index) => (
+                    <option key={index} value={name}>{name}</option> // Using name as both value and display
+                ))}
+        </select>
+        </div>
+        <div className='flex'>
+        <input 
+          type="text" 
+          placeholder="Report Time" 
           className="block w-64 p-2 mb-4 m-2 border border-gray-300 bg-input rounded" 
           required  onChange={(e)=> setUsername(e.target.value)}
         />
         <input 
           type="text" 
-          placeholder="Password" 
+          placeholder="frequency" 
           className="block w-64 p-2 mb-4 m-2 border border-gray-300 bg-input rounded" 
           required  onChange={(e)=> setPassword(e.target.value)}
         />
         </div>
-        <div className='flex'>
-        <input 
-          type="text" 
-          placeholder="Hostname" 
-          className="block w-64 p-2 mb-4 m-2 border border-gray-300 bg-input rounded" 
-          required  onChange={(e)=> setHostname(e.target.value)}
-        />
-        <input 
-          type="text" 
-          placeholder="Port" 
-          className="block w-64 p-2 mb-4 m-2 border border-gray-300 bg-input rounded" 
-          required  onChange={(e)=> setPort(e.target.value)}
-        />
-        </div>
+        
+        <label className='flex items-center w-full '>
+    
+            <textarea
+                type="text" 
+                placeholder="Query" 
+                className="block w-full h-60 p-2 mb-4 m-2 border border-gray-300 bg-input rounded" 
+                required  onChange={(e)=> setHostname(e.target.value)}
+                />
+        </label>
+        
+        
+      
          <button className="w-full p-2 bg-btn-purple text-white rounded hover:bg-blue-600" onClick={handleButtonClick}>
-          Add DataSource
+          Add Report
         </button>
       </div>
     </div>
