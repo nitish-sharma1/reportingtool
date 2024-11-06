09b2e8c75d0eea6d1c520a914b1f71259413d758
Nitish <nitishsharma.sharma2@gmail.com>
2024-11-07 00:39:29 +0530

    added schema for outbound service

---
+++ Schema/outboundServicesSchema.py
@@ -0,0 +1,13 @@
+from marshmallow import Schema, fields
+
+
+class OutboundServiceSchema(Schema):
+    datasource_type = fields.Str(required=True)
+    instance_name = fields.Str(required=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True)
+    hostname = fields.Str(required=True)
+    port = fields.Int(required=True)
+
+    class Meta:
+        strict = True
