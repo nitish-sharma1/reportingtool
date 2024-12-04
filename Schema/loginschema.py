116d58ae70fa3f6b43446bd1dee2fcaadcf84064
Nitish <nitishsharma.sharma2@gmail.com>
2024-12-05 01:58:29 +0530

    adding login schema

---
+++ Schema/loginschema.py
@@ -0,0 +1,9 @@
+from marshmallow import Schema, fields, validate
+
+
+class LoginSchema(Schema):
+    username = fields.Str(required=True, validate=validate.Length(max=150))
+    password = fields.String(required=True, validate=validate.Length(min=8))
+
+    class Meta:
+        strict = True
